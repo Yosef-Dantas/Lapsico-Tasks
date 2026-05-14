f9bfd9b7de0c7ec48af3bfa46db4a5789b533979
Yosef Faustiny Dantas Silva <yosefkely2122@gmail.com>
2026-05-14 17:25:23 -0300

    deleted

--- TASK-1/ISSUE1.py
+++
@@ -1,7 +0,0 @@
-import cv2
-#Imagem foi aberta
-imagem = cv2.imread("C:\\Users\\LABIC\\Desktop\\Yosef Lab\\LAPISCO-TASKS-1\\TASK-1\\arara.jpg")
-#Imagem foi mostrada pelo computador.
-cv2.imshow("Arara: ", imagem)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
