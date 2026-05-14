7be76bd9a8cba6afce4fc3aea36e0bf922d9360b
Yosef Faustiny Dantas Silva <yosefkely2122@gmail.com>
2026-05-14 17:24:57 -0300

    Feat: Task 02 done.

---
+++ TASK-02/ISSUE2.py
@@ -0,0 +1,13 @@
+# Importando a biblioteca.
+import cv2
+# Imagem foi aberta.
+imagem = cv2.imread("C:\\Users\\LABIC\\Desktop\\Yosef Lab\\LAPISCO-TASKS-1\\TASK-2\\arara.jpg")
+# Imagem foi mostrada.
+cv2.imshow("Arara: ", imagem)
+# Imagem foi convertida para cor cinza.
+imagem_cinza = cv2.cvtColor(imagem,cv2.COLOR_BGR2GRAY)
+# Nova imagem foi mostrada e salva pelo computador.
+cv2.imshow("Arara cinza:", imagem_cinza)
+cv2.imwrite("Arara Cinza.jpg", imagem_cinza)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
